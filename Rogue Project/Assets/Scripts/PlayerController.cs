cbd5b1bb23107ce85805c63e4659b92893b176bb
artegful <artegful@users.noreply.github.com>
2020-04-15 17:44:58 +0300

    Moving sprite

---
+++ Rogue Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 5f;
+    public Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        rb.velocity = movement * speed;
+	}
+}
